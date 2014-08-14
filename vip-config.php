488eb0bf7b20976ee0fe2160917dacc880e3be05
Gary Pendergast <gary@pento.net>
2014-08-14 15:08:15 +1000

    Add vip-config.php

---
+++ vip-config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Hi there, VIP dev!
+ *
+ * vip-config.php is where you put things you'd usually put in wp-config.php. Don't worry about database settings
+ * and such, we've taken care of that for you. This is just for if you need to define an API key or something
+ * of that nature.
+ *
+ * Happy Coding!
+ *
+ * - The WordPress.com VIP Team
+ **/
+ 
