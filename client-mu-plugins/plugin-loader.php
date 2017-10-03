8d12c4741797c7cf2e8f2ab7d917e8ee058ef3b1
Simon Wheatley <simonw@automattic.com>
2017-10-03 14:40:28 +0100

    Example plugin loader file (#19)

---
+++ client-mu-plugins/plugin-loader.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * We recommend all plugins for your site are
+ * loaded in code, either from a file like this
+ * one or from your theme (if the plugins are 
+ * specific to your theme and do not need to be
+ * loaded as early as this in the WordPress boot
+ * sequence.
+ * 
+ * @see https://vip.wordpress.com/documentation/vip-go/understanding-your-vip-go-codebase/
+ */
+
+// wpcom_vip_load_plugin( 'plugin-name' );
+// Note the above requires a specific naming structure: /plugin-name/plugin-name.php
+// You can also specify a specific root file: wpcom_vip_load_plugin( '/plugin-name/plugin.php' );
