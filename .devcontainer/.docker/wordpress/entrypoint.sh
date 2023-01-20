d9c885f6cfd4de9a008dcc20d52b2dfb57021d94
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-01-20 20:37:23 +0200

    Update login shell

--- .devcontainer/.docker/wordpress/entrypoint.sh
+++ .devcontainer/.docker/wordpress/entrypoint.sh
@@ -4,7 +4,7 @@
 : "${HOST_GID:=1000}"
 
 getent group wordpress || addgroup -g "${HOST_GID}" wordpress
-getent passwd wordpress || adduser -D -h /var/empty -s /sbin/nologin -u "${HOST_UID}" -G wordpress wordpress
+getent passwd wordpress || adduser -D -h /var/empty -u "${HOST_UID}" -G wordpress wordpress
 
 rsync -a /wp/ /shared/
 chown -R wordpress /shared
