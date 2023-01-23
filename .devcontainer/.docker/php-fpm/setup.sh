e219c17e3a2cd7a4db9ac606f821a298c1895357
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-01-23 23:35:55 +0200

    Add port to preview domain

--- .devcontainer/.docker/php-fpm/setup.sh
+++ .devcontainer/.docker/php-fpm/setup.sh
@@ -3,7 +3,7 @@
 DOMAIN="localhost.localdomain"
 
 if [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" ] && [ -n "${CODESPACE_NAME}" ]; then
-    DOMAIN="${CODESPACE_NAME}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+    DOMAIN="${CODESPACE_NAME}.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}-8080"
 fi
 
 /dev-tools/setup.sh database root "http://${DOMAIN}/" "Test"
